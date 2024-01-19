bce5df40dc3847f7dcffc313b1f167c148078d6a
Gabriel Correa <gabrielcorreacardenas@gmail.com>
2024-01-19 17:06:35 -0500

    feat: survey responses numeber in the list

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import "@mantine/core/styles.css";
+import "@mantine/dropzone/styles.css"
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
